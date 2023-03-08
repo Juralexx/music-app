e563e3b52edeef7adf4894846c01678fc6b6eacc
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-08 23:46:21 +0100

    Fetch playlists hook

---
+++ views/src/components/functions/usePlaylists.ts
@@ -0,0 +1,44 @@
+import React from 'react'
+import axios from 'axios'
+
+const usePlaylists = () => {
+    const [playlists, setPlaylists] = React.useState<Array<any>>([])
+
+    React.useEffect(() => {
+        if (playlists.length === 0) {
+            const fetchPlaylists = async () => {
+                await axios({
+                    method: 'GET',
+                    url: `${process.env.REACT_APP_API_URL}/api/fetch-playlists`,
+                    headers: {
+                        'Authorization': process.env.REACT_APP_ACCESS_TOKEN
+                    }
+                })
+                    .then(async res => {
+                        if (res.data.length > 0) {
+                            const promise: any[] = res.data.map(async (file: string) => {
+                                return await axios({
+                                    method: 'GET',
+                                    url: `${process.env.REACT_APP_API_URL}/playlists/${file}`,
+                                    headers: {
+                                        'Authorization': process.env.REACT_APP_ACCESS_TOKEN
+                                    }
+                                })
+                                    .then(response => response.data)
+                                    .catch(err => console.log(err))
+                            })
+                            Promise.all(promise).then(response => {
+                                setPlaylists(response)
+                            })
+                        }
+                    })
+                    .catch(err => console.log(err))
+            }
+            fetchPlaylists()
+        }
+    }, [playlists])
+
+  return { playlists, setPlaylists }
+}
+
+export default usePlaylists
