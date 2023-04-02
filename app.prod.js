e5e93d359239725a7df53f7a7f6a12d18873b5e8
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-02 18:37:15 +0200

    prod server file

---
+++ app.prod.js
@@ -0,0 +1,152 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import { rateLimit } from 'express-rate-limit'
+import compression from 'compression'
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
+import fs from 'graceful-fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+const FRONT_URL = process.env.NODE_ENV !== 'production' ? process.env.DEV_FRONT_URL : process.env.FRONT_URL
+const SERVER_URL = process.env.NODE_ENV !== 'production' ? process.env.DEV_SERVER_URL : process.env.SERVER_URL
+const MUSICS_ROUTE = process.env.NODE_ENV !== 'production' ? process.env.LOCAL_MUSICS_ROUTE : process.env.ONLINE_MUSICS_ROUTE
+
+app.use(cors({
+    'credentials': true,
+    'origin': FRONT_URL,
+    'allowedHeaders': ['Content-Length', 'Content-Type', 'application', 'Authorization'],
+    'methods': 'GET, POST, PUT',
+    'preflightContinue': false,
+}))
+
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+    // crossOriginResourcePolicy: false,
+    crossOriginResourcePolicy: {
+        policy: 'cross-origin'
+    },
+    originAgentCluster: true,
+    contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+            "img-src": ["'self'", "https: data:"],
+            "default-src": ["*"]
+        }
+    }
+}))
+app.use(express.json({ limit: '150kb' }))
+app.use(bodyParser.urlencoded({
+    extended: false,
+    limit: '150kb'
+}))
+app.use(bodyParser.json({ limit: '150kb' }))
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 150,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too Many Request from this IP, your IP has been blocked. Please try again later.'
+})
+
+app.use(compression())
+
+/**
+ * 
+ */
+
+app.use('/api/musics', limiter, express.static(MUSICS_ROUTE))
+app.use('/api/albums', express.static('./albums-compressed'))
+
+const isToken = (req, res, next) => {
+    if (req.get('Authorization') === process.env.ACCESS_TOKEN) {
+        res
+        res.flush()
+        next()
+    } else {
+        return res.status(400).json({ error: 'Vous devez posséder un Token secret pour accéder à cette requête.' })
+    }
+}
+
+app.use('/api/playlists', isToken, express.static('./playlists'))
+app.use('/api/datas', isToken, express.static('./data/datas.json'))
+
+const router = express.Router()
+
+router.get('/api/fetch-playlists', isToken, (req, res) => {
+    fs.readdir('./playlists', (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(files)
+        }
+    })
+})
+
+router.post('/api/add-playlist', isToken, (req, res) => {
+    const { playlist } = req.body
+    fs.writeFile(`./playlists/${playlist._id}.json`, JSON.stringify(playlist), err => {
+        if (err)
+            console.error(err);
+        else {
+            return res.status(200).end()
+        }
+    });
+})
+
+router.put('/api/update-playlist', isToken, (req, res) => {
+    const { playlist } = req.body
+    fs.unlinkSync(`./playlists/${playlist._id}.json`, err => {
+        if (err)
+            return res.status(400).json({ error: 'Une erreur est survenue' })
+    })
+    fs.writeFile(`./playlists/${playlist._id}.json`, JSON.stringify(playlist), err => {
+        if (err)
+            return res.status(400).json({ error: 'Une erreur est survenue' })
+        else {
+            return res.status(200).end()
+        }
+    });
+})
+
+router.put('/api/delete-playlist', isToken, (req, res) => {
+    const { playlist } = req.body
+    fs.unlinkSync(`./playlists/${playlist._id}.json`, err => {
+        if (err)
+            return res.status(400).json({ error: 'Une erreur est survenue' })
+        else return res.status(200).end()
+    })
+})
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, `./build/`)))
+
+    router.get('/*', (_, res) => {
+        res.sendFile(path.join(__dirname, './build/index.html'))
+    });
+}
+
+app.use('/', router)
+
+/**
+ * 
+ */
+
+if (process.env.NODE_ENV !== 'production') {
+    process.once('uncaughtException', err => {
+        console.error(err.stack || err)
+        setTimeout(() => process.exit(1), 100)
+    })
+}
+
+const PORT = process.env.PORT || 5002
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré : http://localhost:${PORT}`)
+})
